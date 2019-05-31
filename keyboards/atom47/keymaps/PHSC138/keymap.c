36a1a9c21aa58cef422acbeaa8b3226d288245ba
PHSC138 <PHSC138@users.noreply.github.com>
2019-05-30 23:23:46 -0400

    changed arrows to vim arrows

--- keyboards/atom47/keymaps/PHSC138/keymap.c
+++ keyboards/atom47/keymaps/PHSC138/keymap.c
@@ -14,10 +14,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL,		KC_LGUI,	KC_LALT,	TG(_L3),	KC_SPC, 				KC_SPC,					MO(_L2),	KC_RALT,	KC_APP,					KC_RCTRL),	\
  
 [_L2] = LAYOUT(
-  _______,		KC_VOLD,	KC_VOLU,	KC_MUTE,	RESET,		_______,	KC_CALC,	KC_PGUP,	KC_UP,		KC_PGDN,	KC_PSCR,	KC_SLCK,	KC_PAUS,	\
-  KC_CAPS,		KC_MPRV,	KC_MPLY,	KC_MNXT,	_______,	_______,	KC_HOME,	KC_LEFT,	KC_DOWN,	KC_RIGHT,	KC_INS,					_______,	\
-  _______,		_______,	_______,	_______,	_______,	_______,	_______,	BL_TOGG,	BL_DEC,		BL_INC,		_______,				_______,	\
-  _______,		_______,	_______,	_______,	_______, 				_______,				_______,	_______,	_______,				_______),	\
+  _______,		KC_VOLD,	KC_VOLU,	KC_MUTE,	RESET,		_______,	KC_CALC,	KC_PGUP,	______,		KC_PGDN,	KC_PSCR,	KC_SLCK,	KC_PAUS,	\
+  KC_CAPS,		KC_MPRV,	KC_MPLY,	KC_MNXT,	_______,	_______,	KC_LEFT,	KC_DOWN,	KC_UP,	KC_RIGHT,	KC_INS,					_______,	\
+  _______,		_______,	_______,	_______,	_______,	_______,	KC_HOME,	BL_TOGG,	BL_DEC,		BL_INC,		_______,				_______,	\
+  _______,		_______,	_______,	_______,	_______, 				_______,				,	_______,	_______,				_______),	\
  
 [_L1] = LAYOUT(
   KC_GRV,		KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,		\
